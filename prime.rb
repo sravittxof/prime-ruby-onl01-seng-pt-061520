5bea9e2d836b86621047691a928123f0067979ee
Travis Fox <travis.fox90@gmail.com>
2020-06-27 19:16:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  elsif num == 2
+    return true
+  elsif num.even?
+    return false
+  else
+    array = (3...num).to_a
+    if array.any? {|i| num%i == 0 }
+      return false
+    else
+      return true
+    end
+  end
+end
+
